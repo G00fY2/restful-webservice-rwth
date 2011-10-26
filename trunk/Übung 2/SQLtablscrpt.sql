575fce679d0b317bbf43448ea6e758938cf3e694
thomasf.wirth@gmail.com <thomasf.wirth@gmail.com@2d5b8323-6e8e-26da-8047-d0f3d664b30c>
2011-10-26 18:13:54 +0000

    aktuelle Übungslösungen

---
+++ trunk/Übung 2/SQLtablscrpt.sql
@@ -0,0 +1,65 @@
+CREATE TABLE User( 
+  Passwort varchar(10) NOT NULL, 
+  Benutzername varchar(30) NOT NULL, 
+  EP int(11), 
+  Vorname varchar(30) NOT NULL,
+  Nachname varchar(30) NOT NULL,
+  EMail varchar(50) NOT NULL,
+  
+  CONSTRAINT User_PK PRIMARY KEY (EMail)
+); 
+
+
+
+
+CREATE TABLE Picture( 
+  Beschreibung varchar(100) NOT NULL, 
+  Value int(1) NOT NULL, 
+  URL varchar(100) NOT NULL, 
+  
+  CONSTRAINT Picture_PK PRIMARY KEY (URL), 
+  CONSTRAINT Picture_Value_C CHECK (Vaulue >=0 AND Value <=1)
+); 
+
+
+
+
+CREATE TABLE Achievement( 
+  Bild varchar(100) NOT NULL, 
+  Beschreibung varchar(100) NOT NULL, 
+  Name varchar(40) NOT NULL, 
+  Identifier int(11) NOT NULL, 
+  
+  CONSTRAINT Achievement_PK PRIMARY KEY (Identifier)
+); 
+
+
+
+
+CREATE TABLE rates( 
+  RatesID int(11) NOT NULL,
+  Zeit time NOT NULL,
+  Rate int(1) NOT NULL, 
+  FKURL varchar(100) NOT NULL, 
+  FKEMail varchar(50) NOT NULL,
+
+  CONSTRAINT rates_PK PRIMARY KEY (RatesID),
+  CONSTRAINT M_FK FOREIGN KEY (FKEMail) REFERENCES User (EMail),
+  CONSTRAINT U_FK FOREIGN KEY (FKURL) REFERENCES Picture (URL)
+);
+
+
+
+CREATE TABLE collect( 
+  CollectID int(11) NOT NULL,
+  Anzahl int(11) NOT NULL, 
+  FKMail varchar(50) NOT NULL,
+  FKIdentifier int(11) NOT NULL, 
+
+  CONSTRAINT collect_PK PRIMARY KEY (CollectID),
+  CONSTRAINT M2_FK FOREIGN KEY (FKMail) REFERENCES User (EMail),
+  CONSTRAINT I_FK FOREIGN KEY (FKIdentifier) REFERENCES Achievement(Identifier)
+);
+
+
+
