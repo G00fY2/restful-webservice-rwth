d4ca76467df697ff355033c8ac4ce8755e814415
Hausburg.Sven@gmail.com <Hausburg.Sven@gmail.com@2d5b8323-6e8e-26da-8047-d0f3d664b30c>
2011-11-03 13:35:50 +0000

    getBy - Methoden angepasst

--- trunk/Übung 3/ugnm-service-skeleton/src/main/java/de/rwth/dbis/ugnm/service/UserService.java
+++ trunk/Übung 3/ugnm-service-skeleton/src/main/java/de/rwth/dbis/ugnm/service/UserService.java
@@ -7,8 +7,9 @@ import de.rwth.dbis.ugnm.entity.User;
 public interface UserService {
 	public boolean save(User user);
 	public List<User> getAll();
-	public User getById(int id);
-	public User getByLogin(String login);
+	public User getByBenutzername(String benutzername);
+	public User getByVorname(String vorname);
+	public User getByNachname(String nachname);
 	public boolean delete(User user);
 	public boolean update(User user);
 	public User findUser(User user);
