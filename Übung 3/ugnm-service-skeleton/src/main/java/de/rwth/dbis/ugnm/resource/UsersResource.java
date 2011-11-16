7a7660eb664c7679a6b86cae6292d70b58aa49c9
Hausburg.Sven <Hausburg.Sven@gmail.com>
2011-11-16 18:59:36 +0000

    import überprüft - final! müsste laufen

--- Übung 3/ugnm-service-skeleton/src/main/java/de/rwth/dbis/ugnm/resource/UsersResource.java
+++ Übung 3/ugnm-service-skeleton/src/main/java/de/rwth/dbis/ugnm/resource/UsersResource.java
@@ -1,20 +1,20 @@
 package de.rwth.dbis.ugnm.resource;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.WebApplicationException;
 
